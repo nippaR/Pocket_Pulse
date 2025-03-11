3860f4b1e9bda8820da1243727c6860ac9ac29cb
Thanuja Nipun <thanujanipun6@gmail.com>
2025-03-11 23:01:52 +0530

    Installed MUI Library and add Btn Components and home page

---
+++ client/pocket-pulse/src/components/Button.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Button } from '@mui/material';
+
+export default function BTN() {
+  return (
+    <Button variant="contained" color="secondary">Click Me</Button>
+  );
+}
