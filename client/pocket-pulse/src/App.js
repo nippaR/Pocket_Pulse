bdb919b80ab03641ebbf2a8fb7b477204d79325e
Thanuja Nipun <thanujanipun6@gmail.com>
2025-03-14 23:20:48 +0530

    modified app.js, home.js file and newlly added metamask login page file

--- client/pocket-pulse/src/App.js
+++ client/pocket-pulse/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter,Routes, Route} from 'react-router-dom';
 import Header from './pages/Header';
 import Home from './pages/Home';
+import Metamask from './pages/Metamasklog';
 
 function App() {
   return(
@@ -9,6 +10,7 @@ function App() {
         <Header/>
         <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Metamask/>} />
         </Routes>
     </BrowserRouter>
   );
